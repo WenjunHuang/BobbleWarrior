71148c5f4adab1aabc9bd7ba3731f69f83e143b4
WenjunHuang <wenjun.huang@gmail.com>
2017-04-04 12:26:06 +0800

    Chapter 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,57 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     public Rigidbody head;
+    public LayerMask layerMask;
+
+    private Vector3 currentLookTarget = Vector3.zero;
+
 
     private CharacterController _characterController;
 
-    private void Start()
+    public void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
 
-    private void Update()
+    public void Update()
     {
         var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _characterController.SimpleMove(moveDirection * moveSpeed);
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
+    {
+        AddForceToHead();
+        AdjustOrientation();
+    }
+
+    private void AdjustOrientation()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+        }
+
+        var targetPosition = new Vector3
+        {
+            x = hit.point.x,
+            y = transform.position.y,
+            z = hit.point.z
+        };
+        var rotation = Quaternion.LookRotation(targetPosition - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+    }
+
+    private void AddForceToHead()
     {
         var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if (moveDirection == Vector3.zero)
-        {
-
-        }
-        else
+        if (moveDirection != Vector3.zero)
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
