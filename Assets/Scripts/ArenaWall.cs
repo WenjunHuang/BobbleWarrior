ce38570ff42b8c3d44caf111280eca93436c1fb4
WenjunHuang <wenjun.huang@gmail.com>
2017-04-05 22:13:44 +0800

    wall animations

---
+++ Assets/Scripts/ArenaWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour
+{
+
+    private Animator _arenaAnimator;
+	public void Start ()
+	{
+	    var arena = transform.parent.gameObject;
+	    _arenaAnimator = arena.GetComponent<Animator>();
+	}
+	
+	public void Update () 
+	{
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        _arenaAnimator.SetBool("IsLowered", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _arenaAnimator.SetBool("IsLowered", false);
+    }
+}
